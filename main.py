dd5e8d70d2d9441fb8b00e165bd109b39e78ad46
Ramji67 <158837797+Ramji67@users.noreply.github.com>
2025-02-22 19:29:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,14 +39,14 @@ bot = Client(
     bot_token=BOT_TOKEN
 )
 
-API_ID    = os.environ.get("API_ID", "24495656")
-API_HASH  = os.environ.get("API_HASH", "61afcf68c6429714dd18acd07f246571")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8040014845:AAHAo3zgpikp-4j2PLQofMxSwtoOdQKTQLg") 
+API_ID    = os.environ.get("API_ID", "28088290")
+API_HASH  = os.environ.get("API_HASH", "6998f2c585fdce65ac72dfa23d02b6ec")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
 # Get the MongoDB collection for this bot
 collection = get_collection(BOT_NAME, MONGO_URI)
 # Constants
-OWNER_IDS = [5548106944]  # Replace with the actual owner user IDs
+OWNER_IDS = [5850397219]  # Replace with the actual owner user IDs
 
 cookies_file_path = "modules/cookies.txt"
 # Global variables
